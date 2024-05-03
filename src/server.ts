d4fcae2e215344a20e5b0d3c0830a0f28b0c7636
AbdujalolAndy <mrarnoldunlimited@gmail.com>
2024-05-04 04:02:08 +0900

    feat: Envorimental Variables & creating train file

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,3 @@
-import moment from "moment";
-const currentTime = moment().format("YYYY MM DD")
-console.log("Execution!")
-console.log(currentTime)
+import dotenv from "dotenv";
+dotenv.config();
 
-interface Person {
-    name: string;
-    age: number;
-    applied_job: boolean
-}
-const obj: Person = {
-    name: "Andy",
-    age: 23,
-    applied_job: false
-}
